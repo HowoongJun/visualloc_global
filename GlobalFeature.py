2db9de3c502e8dcf3ada596027ade76b590ed06e
Howoong Jun <prestoxic@gmail.com>
2021-03-02 20:50:11 +0900

    [UPD] submodule update - GeM

--- GlobalFeature.py
+++ GlobalFeature.py
@@ -9,6 +9,9 @@ class CVisualLocGlobal():
         elif model == "netvlad":
             print("Model: NetVLAD")
             self.__module = imp.load_source(model, "./globalfeature_ref/netvlad/netvlad.py")
+        elif model == "gem":
+            print("Model: GeM")
+            self.__module = imp.load_source(model, "./globalfeature_ref/gem/gem.py")
 
     def __del__(self):
         print("GlobalFeature Destructor!")

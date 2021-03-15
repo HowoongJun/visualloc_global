7cbe5871b26a5dba9a91373f99a1d258e6547db2
HowoongJun <prestoxic@gmail.com>
2021-03-15 16:51:13 +0900

    [MOD] query test add and test data changed

--- testGlobal.py
+++ testGlobal.py
@@ -6,11 +6,12 @@ import argparse
 from glob import glob
 import tensorflow.compat.v1 as tf
 import common.Log as log
+import numpy as np
 
 parser = argparse.ArgumentParser(description='Test Global Feature')
 parser.add_argument('--model', '--m', type=str, default='mymodule', dest='model',
                     help='Model select: mymodule, netvlad, gem, cam')
-parser.add_argument('--db', '--d', default=None, dest='db',
+parser.add_argument('--db', '--d', type=str, dest='db',
                     help='DB creation mode: DB file save path')
 parser.add_argument('--path', '--p', type=str, dest='path',
                     help='Image file path')
@@ -18,6 +19,8 @@ parser.add_argument('--resize', '--z', default = '[1280,720]', dest='resize',
                     help='Resize image [width, height] (default = [1280,720]')
 parser.add_argument('--channel', '--c', type=int, default = 3, dest='channel',
                     help='Image channel')
+parser.add_argument('--mode', '--o', type=str, dest='mode',
+                    help='Mode select: makedb, query, match')
 args = parser.parse_args()
 
 def readFolder(strImgFolder):
@@ -34,12 +37,14 @@ def makeDB(oModel) -> bool:
     strFileList = readFolder(args.path)
     if(strFileList is False):
         return False
+    print(len(strFileList))
     for fileIdx in strFileList:
         strImgPath = args.path + '/' + fileIdx
         oModel.Setting(eSettingCmd.eSettingCmd_IMAGE_DATA, imageRead(strImgPath))
         if(oModel.Write(args.db, fileIdx) is False):
-            log.DebugPrint().error("Fail to write DB for image " + fileIdx)
+            log.DebugPrint().error("Fail to write DB for image " + str(fileIdx))
             return False
+        log.DebugPrint().info(str(fileIdx) + " Image finished!")
         oModel.Reset()
     return True
 
@@ -50,10 +55,34 @@ def queryCheck(oModel):
     for fileIdx in strFileList:
         strImgPath = args.path + '/' + fileIdx
         oModel.Setting(eSettingCmd.eSettingCmd_IMAGE_DATA, imageRead(strImgPath))
-        result = oModel.Read()
-        print(result)
+        vResult = oModel.Read()
+        print(vResult)
         oModel.Reset()
     return True
+
+def queryMatch(oModel):
+    strQueryList = readFolder(args.path)
+    strDbPath = args.db + '/' + args.model + '/'
+    strDbList = [os.path.basename(x) for x in glob(strDbPath + '*.npy')]
+    if(strQueryList is False):
+        return False
+    for fileIdx in strQueryList:
+        strQueryPath = args.path + '/' + fileIdx
+        oModel.Setting(eSettingCmd.eSettingCmd_IMAGE_DATA, imageRead(strQueryPath))
+        vResult = oModel.Read()
+        vEucDist = []
+        vMatchedImage = []
+        for dbIdx in strDbList:
+            strNpyPath = strDbPath + dbIdx
+            vDbResult = np.load(strNpyPath)
+            fEucDist = np.linalg.norm(np.abs(vDbResult - vResult))
+            vEucDist.append(fEucDist)
+
+        for idxTopN in range(0, 5):
+            minIdx = np.argmin(vEucDist)
+            vMatchedImage.append(strDbList[minIdx].replace('.npy', ''))
+            vEucDist[minIdx] = float('inf')
+        log.DebugPrint().info(str(fileIdx) + ": " + str(vMatchedImage))
 
 def imageRead(strImgPath):
     oImage = cv2.imread(strImgPath)
@@ -82,11 +111,16 @@ if __name__ == "__main__":
     model.Open()
     model.Setting(eSettingCmd.eSettingCmd_IMAGE_CHANNEL, args.channel)
     model.Setting(eSettingCmd.eSettingCmd_CONFIG, checkGPU)
-    if(args.db is not None):
+    if(args.mode == "makedb"):
         log.DebugPrint().info("DB Creation Mode!")
         if(makeDB(model) is False):
             log.DebugPrint().error("DB Creation Error!")
-    else:
+    elif(args.mode == "query"):
         log.DebugPrint().info("Query mode!")
         if(queryCheck(model) is False):
             log.DebugPrint().error("Query Mode Error!")
+    elif(args.mode == "match"):
+        log.DebugPrint().info("Matching mode!")
+        if(queryMatch(model) is False):
+            log.DebugPrint().error("Matching Mode Error!")
+        
